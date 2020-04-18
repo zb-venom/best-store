6ae7357dbf5ea06f683192b3fff7fbfa9fd83832
ZB Tyoma <zb.tyoma@gmail.com>
2020-04-18 15:09:47 +0700

    config

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('./public'));
 app.use(appRoutes)
 app.use(cookieParser('secret key'))
 
