948fe493010745dc05851e286bbece6e632145cc
ZB Tyoma <zb.tyoma@gmail.com>
2020-04-18 14:40:19 +0700

    config

---
+++ config/default.js
@@ -0,0 +1,11 @@
+var config = {}
+
+config.server = {
+    port: 7777,
+}
+
+config.db = {
+    key: 'mongodb+srv://admin:MVPmu8Mku9WfKreG@cluster0-geu5y.mongodb.net/best-store',
+}
+
+module.exports = config;
