94fb1ac26fc6b615da34f0ed3df352432bda0924
ZB Tyoma <34129397+zb-venom@users.noreply.github.com>
2021-09-18 13:31:34 +0300

    Update default.js

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ config.server = {
 }
 
 config.db = {
-    key: 'mongodb+srv://admin:admin@cluster0.geu5y.mongodb.net/best-store',
+    key: 'mongodb+srv://admin:6jbpAslczEFaeKDj@cluster0.geu5y.mongodb.net/best-store',
 }
 
 module.exports = config;
